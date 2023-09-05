f5b68cf29f4072e8ed21be8419ff2a249565a046
karolyleo <leonard.a.karoly@gmail.com>
2023-09-05 17:46:55 -0500

    Tree lab

--- DSA/LearnImmutable/SampleRecordTest.cs
+++ DSA/LearnImmutable/SampleRecordTest.cs
@@ -1,71 +1,32 @@
-namespace LearnImmutable
+namespace LearningImmutable
 {
     [TestClass]
-    public class SampleRecordTest
+    public class DirectoryTreeTest
     {
         [TestMethod]
-        public void TestEqualParams()
+        public void TestValidRoots()
         {
             //Arange
-            SampleRecord r1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
-            SampleRecord r2 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
+            var builder = new DirectoryTreeBuilder();
+            string invalidDirectory = "InvalidDirectory";
+            //Assert
+            Assert.ThrowsException<DirectoryNotFoundException>(() => builder.BuildTree(invalidDirectory, "Endpoint"));
+        }
+
+        [TestMethod]
+        public void BuildTreeValidInputs()
+        {
+            //Arrange
+            var builder = new DirectoryTreeBuilder();
+            string rootDir = "C:\\MSSA projects";
+            string endpoint = "C:\\MSSA projects\\stack-overflow repo\\DSA\\LearningImmutable";
+            DirectoryTreeBuilder.TreeNode rootNode = builder.BuildTree(rootDir, endpoint);
 
             //Assert
-            Assert.AreEqual(r1, r2);
-            Assert.AreNotSame(r1, r2);
-        }
-        [TestMethod]
-        public void TestDiffParams()
-        {
-            //Arange
-            SampleRecord r1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
-            SampleRecord r2 = new SampleRecord(ParamString: "Tess", ParamInt: 3, ParamDate: new DateTime(2023, 4, 5));
+            Assert.IsNotNull(rootNode);
+            Assert.AreEqual("MSSA projects", rootNode.Name);
 
-            //Assert
-            Assert.AreNotEqual(r1, r2);
-            Assert.AreNotSame(r1, r2);
-        }
-        [TestMethod]
-        public void TestSamePosition()
-        {
-            //Arange
-            SampleRecord r1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
-            SampleRecord r2 = r1;
-
-            //Assert
-            Assert.AreEqual(r1, r2);
-            Assert.AreSame(r1, r2);
-        }
-        [TestMethod]
-        public void TestRecordByProperty()
-        {
-            //arrange
-            SampleRecord r1 = new SampleRecord(ParamString: "Test", ParamInt: 1, ParamDate: new DateTime(2023, 9, 5));
-
-            //assert
-            Assert.AreEqual("Test", r1.ParamString);
-            Assert.AreEqual(1, r1.ParamInt);
-            Assert.AreEqual(new DateTime(2023, 9 , 5), r1.ParamDate);
-
-        }
-        [TestMethod]
-        public void TestPeople()
-        {
-            Person pvt = new Person("Leo", "K.");
-            Person sgt = new Person("Johnny B.", "Goode");
-
-            Assert.AreNotEqual(pvt, sgt);
-        }
-        [TestMethod]
-        public void TestingPersons()
-        {
-            Person p1 = new("Nancy", "Davolio") { PhoneNumbers = new string[1] };
-            Person p2 = p1 with { FirstName = "John" };
-            p2 = p1 with { PhoneNumbers = new string[1] };
-            Assert.AreNotEqual(p1, p2);
-            Assert.AreNotSame(p1, p2);
-            Assert.AreEqual("Nancy", p2.FirstName.ToString());
-            //Assert.AreSame(p2.LastName, p2.LastName);
+            Console.WriteLine(rootNode.Name);
         }
     }
 }
--- DSA/LearningImmutable/SampleRecord.cs
+++ DSA/LearningImmutable/SampleRecord.cs
@@ -1,14 +1,48 @@
-﻿namespace LearnImmutable
-{
-    public record class SampleRecord(string ParamString, int ParamInt, DateTime ParamDate);
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
-    public record Person(string FirstName, string LastName)
+namespace LearnImmutable
+{
+    public class DirectoryTreeBuilder
     {
-        public string[] PhoneNumbers { get; init; }
-    }
-    /*public record Person
-    {
-        public required string FirstName { get; init; }
-        public required string LastName {  get; init; }
-    };*/
+        public class TreeNode
+        {
+            public string Name { get; }
+            public List<TreeNode> Children { get; } = new List<TreeNode>();
+
+            public TreeNode(string name)
+            {
+                Name = name;
+            }
+        }
+
+            public TreeNode BuildTree(string rootDirectory, string endpoint)
+            {
+                if (!Directory.Exists(rootDirectory))
+                {
+                    throw new DirectoryNotFoundException($"Directory not found: {rootDirectory}");
+                }
+
+                return BuildTreeRecursive(rootDirectory, endpoint);
+            }
+
+            private TreeNode BuildTreeRecursive(string directory, string endpoint)
+            {
+                var currentNode = new TreeNode(Path.GetFileName(directory));
+
+                foreach (var subDirectory in Directory.GetDirectories(directory))
+                {
+                    if (subDirectory == endpoint)
+                    {
+                        continue;
+                    }
+
+                    var childNode = BuildTreeRecursive(subDirectory, endpoint);
+                    currentNode.Children.Add(childNode);
+                }
+                return currentNode;
+            }
+        }
+
 }
